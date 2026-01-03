81d03db6ca30ece559b3d6b6111502144cdb832f
LitapTimus <sumitpatil141005@gmail.com>
2026-01-03 14:35:45 +0530

    deployment

--- main.py
+++ main.py
@@ -5,33 +5,17 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 import sys
 
 if __name__=='__main__':
     try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("Initiate the data ingestion")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
+        
 
-        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
-        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
-        logging.info("Initiate the data validation")
-        data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("Data Validation Completed")
-        print(data_validation_artifact)
+        pipeline = TrainingPipeline()
+        pipeline.run_pipeline()
 
-        datatransformationconfig=DataTransformationConfig(trainingpipelineconfig)
-        data_transformation=DataTransformation(data_validation_artifact,datatransformationconfig)
-        logging.info("Initiate the data transformation")
-        data_transformation_artifact=data_transformation.initiate_data_transformation()
-        logging.info("Data Transformation Completed")
-        print(data_transformation_artifact)
 
     except Exception as e:
            raise NetworkSecurityException(e,sys)
